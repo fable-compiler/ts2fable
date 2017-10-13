9843e00a605832f9c29b77c86ed43d16e6abcbb8
Cameron Taggart <cameron.taggart@gmail.com>
2017-10-13 15:33:37 +0100

    Fable.Import.Node working

--- src/App.fs
+++ src/App.fs
@@ -3,17 +3,8 @@ module FableApp
 open Fable.Core
 open Fable.Core.JsInterop
 open Fable.Import
+open Fable.Import.Node
 
-let init() =
-    let canvas = Browser.document.getElementsByTagName_canvas().[0]
-    canvas.width <- 1000.
-    canvas.height <- 800.
-    let ctx = canvas.getContext_2d()
-    // The (!^) operator checks and casts a value to an Erased Union type
-    // See http://fable.io/docs/interacting.html#Erase-attribute
-    ctx.fillStyle <- !^"rgb(200,0,0)"
-    ctx.fillRect (10., 10., 55., 50.)
-    ctx.fillStyle <- !^"rgba(0, 0, 200, 0.5)"
-    ctx.fillRect (30., 30., 55., 50.)
+printfn "hello"
 
-init()
+Globals.process.exit(1)
--- webpack.config.js
+++ webpack.config.js
@@ -49,5 +49,6 @@ module.exports = {
         },
       }
     ]
-  }
+  },
+  target: 'node'
 };
