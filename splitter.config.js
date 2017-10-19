020d63178962b35eea35d7822468db395e07f09d
Cameron Taggart <cameron.taggart@gmail.com>
2017-10-19 12:32:40 +0100

    switch to fable-splitter instead of webpack

---
+++ splitter.config.js
@@ -0,0 +1,34 @@
+/// @ts-check
+const path = require("path");
+const fableUtils = require("fable-utils");
+
+function resolve(filePath) {
+  return path.resolve(__dirname, filePath)
+}
+
+function runScript(scriptPath) {
+  var scriptDir = path.dirname(scriptPath);
+  // Delete files in directory from require cache
+  Object.keys(require.cache).forEach(function(key) {
+    if (key.startsWith(scriptDir))
+      delete require.cache[key]
+  })
+  try {
+    require(scriptPath);
+  }
+  catch (err) {
+    console.error(err);
+  }
+}
+
+var outFile = resolve("dist/ts2fable.js");
+
+module.exports = {
+  entry: resolve("src/ts2fable.fsproj"),
+  outDir: path.dirname(outFile),
+  babel: fableUtils.resolveBabelOptions({
+    plugins: ["transform-es2015-modules-commonjs"]
+  }),
+  fable: { define: ["DEBUG"] },
+  postbuild() { runScript(outFile) }
+};
