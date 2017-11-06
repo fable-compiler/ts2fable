e50b36908f7c8dcc8979259ec861277a328ec1b9
Cameron Taggart <cameron.taggart@gmail.com>
2017-11-06 13:01:38 -0300

    build on appveyor

---
+++ build.ps1
@@ -0,0 +1,33 @@
+$version = '0.2.0' # the version under development, update after a release
+$versionSuffix = '-local.1' # manually incremented for local builds
+
+function isVersionTag($tag){
+    $v = New-Object Version
+    [Version]::TryParse($tag, [ref]$v)
+}
+
+if ($env:appveyor){
+    $versionSuffix = '-build.' + [int]::Parse($env:appveyor_build_number).ToString()
+    if ($env:appveyor_repo_tag -eq 'true' -and (isVersionTag($env:appveyor_repo_tag_name))){
+        $version = $env:appveyor_repo_tag_name
+        $versionSuffix = ''
+    }
+    Update-AppveyorBuild -Version "$version$versionSuffix"
+}
+
+$v = "$version$versionSuffix"
+
+yarn
+
+Set-Location $psscriptroot\src
+dotnet fable npm-build
+
+Set-Location $psscriptroot
+yarn version --new-version $v --no-git-tag-version
+
+$js = "dist/ts2fable.js"
+@("#!/usr/bin/env node") + (get-content $js) | set-content $js
+
+# yarn pack is packing to many files
+npm pack
+tar tf ts2fable-$v.tgz
