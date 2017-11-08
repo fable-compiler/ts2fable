c124e4274ca3b527987fefd2c93aab80e3f90746
Cameron Taggart <cameron.taggart@gmail.com>
2017-11-08 15:20:59 -0300

    split off core from the app

--- src/tests.fs
+++ src/tests.fs
@@ -7,9 +7,12 @@ let mocha: Mocha.IExports = importDefault "Mocha"
 let assertt: Chai.Chai.Assert = import "assert" "Chai"
 
 mocha.describe.Invoke("my suite", fun _ ->
-    mocha.it.Invoke("my test", fun donee -> 
+    mocha.it.Invoke("my test", fun donee ->
         assertt.equal(1, 1)
         donee.Invoke()
     )|> ignore
     
 ) |> ignore
+
+// let makeItCompile() =
+//     App.forTesting |> ignore
