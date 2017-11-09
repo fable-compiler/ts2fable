95038ad8d999223377eeeb9f5d27e0f610a334a8
Cameron Taggart <cameron.taggart@gmail.com>
2017-11-09 08:32:11 -0300

    follow testing from Fable PowerPack split of core

--- test/test.fs
+++ test/test.fs
@@ -1,21 +1,32 @@
 module ts2fable.Tests
 
+open Fable.Core
 open Fable.Core.JsInterop
 open Fable.Import
 
-let mocha: Mocha.IExports = importDefault "Mocha"
-let assertt: Chai.Chai.Assert = import "assert" "Chai"
+// let mocha: Mocha.IExports = importDefault "Mocha"
+// let assertt: Chai.Chai.Assert = import "assert" "Chai"
 
-mocha.describe.Invoke("my suite", fun _ ->
-    mocha.it.Invoke("my test", fun donee ->
-        assertt.equal(1, 1)
-        donee.Invoke()
-    )|> ignore
-) |> ignore
+[<Global>]
+let describe (msg: string) (f: unit->unit): unit = jsNative
 
-mocha.describe.Invoke("keywords", fun _ ->
-    mocha.it.Invoke("done", fun donee ->
-        assertt.equal(Keywords.escapeWord "done", "``done``")
-        donee.Invoke()
-    )|> ignore
-) |> ignore
+[<Global>]
+let it (msg: string) (f: unit->unit): unit = jsNative
+
+let inline equal (expected: 'T) (actual: 'T): unit =
+    let assert' = importAll<obj> "assert"
+    assert'?deepStrictEqual(actual, expected) |> ignore
+
+
+describe "my tests" <| fun _ ->
+
+    it "addition" <| fun _ ->
+        1 + 2
+        |> equal 3
+
+
+describe "keyword tests" <| fun _ ->
+
+    it "done is escaped" <| fun _ ->
+        Keywords.escapeWord "done"
+        |> equal "``done``"
