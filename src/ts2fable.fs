03f40b2ed7d0dea3d6bf751dca78a3f482415ad7
Cameron Taggart <cameron.taggart@gmail.com>
2017-11-10 13:01:40 -0300

    print out TODO for not yet supported ParamArray

--- src/ts2fable.fs
+++ src/ts2fable.fs
@@ -85,6 +85,7 @@ if argv |> List.exists (fun s -> s = "splitter.config.js") then // run from buil
     writeFile "node_modules/typescript/lib/lib.es2015.promise.d.ts" "test-compile/Fable.Import.Promise.fs"
     writeFile "node_modules/@types/mocha/index.d.ts" "test-compile/Fable.Import.Mocha.fs"
     writeFile "node_modules/@types/chai/index.d.ts" "test-compile/Fable.Import.Chai.fs"
+    writeFile "node_modules/@types/jquery/index.d.ts" "test-compile/Fable.Import.JQuery.fs"
 
 else
     let tsfile = argv |> List.tryFind (fun s -> s.EndsWith ".ts")
