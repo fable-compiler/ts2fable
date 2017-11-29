e34a31483b5c47c13e513b68686c00a52c3000e7
Cameron Taggart <cameron.taggart@gmail.com>
2017-11-29 12:02:23 -0300

    remove engines section from packages.json

--- build-update-package.js
+++ build-update-package.js
@@ -2,4 +2,5 @@
 const fs = require('fs');
 var json = require('./package.json');
 json.scripts = undefined;
+json.engines = undefined;
 fs.writeFileSync("./package.json", JSON.stringify(json, undefined, 2));
