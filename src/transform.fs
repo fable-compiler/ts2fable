bf1cd17ea7de8fffcec349fcd99cd60ac75efbcd
humhei <humhei@outlook.com>
2018-01-30 13:14:29 +0800

    add some comments

--- src/transform.fs
+++ src/transform.fs
@@ -815,6 +815,9 @@ let extractTypeLiterals(f: FsFile): FsFile =
 
                         [it2] @ (List.ofSeq newTypes) // append new types
                     
+                    // compile ts export declare const alias
+                    // to an global variable and a anonymous interface type suffixed with single quotation
+                    // test/fragments/synctasks/f1.d.ts
                     | FsType.Variable vb when vb.HasDeclare && vb.IsConst ->
                         match vb.Type with 
                         | FsType.TypeLiteral tl -> 
@@ -840,6 +843,8 @@ let extractTypeLiterals(f: FsFile): FsFile =
                         | _ -> [tp]    
 
                     | FsType.Alias al -> 
+                        // lift TypeLiteral in union type
+                        // test/fragments/react/f1.d.ts
                         match al.Type with 
                         | FsType.Union un -> 
                             let un2 = 
@@ -854,6 +859,10 @@ let extractTypeLiterals(f: FsFile): FsFile =
                                         tps |> List.ofSeq    
                                 }
                             {al with Type = un2 |> FsType.Union} |> FsType.Alias |> List.singleton
+                        
+                        // ts export declare type should be compild to interface type
+                        // as it has multiple property
+                        // test/fragments/synctasks/f2.d.ts
                         | FsType.TypeLiteral tl ->  
                             {
                                 Comments = []
