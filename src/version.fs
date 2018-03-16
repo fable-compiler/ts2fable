0f543854a6c1502498f31918adea7fac32d035ab
Cameron Taggart <cameron.taggart@gmail.com>
2018-03-15 22:13:07 -0500

    get the version from the package.json fix #199

--- src/version.fs
+++ src/version.fs
@@ -8,6 +8,6 @@ type PackageJson =
         version: string
     }
 let version =
-    let packageJsonPath = path.join(ResizeArray([``process``.cwd(); "package.json"]))
+    let packageJsonPath = path.join(ResizeArray([__dirname; "../package.json"]))
     let packageJson = fs.readFileSync(!^(!^packageJsonPath), !^"utf8") |> ofJson<PackageJson>
     packageJson.version
