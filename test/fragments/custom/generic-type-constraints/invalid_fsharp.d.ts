0625fb52afc3dd9e92750e997883d46aec0a661a
BooksBaum <15612932+Booksbaum@users.noreply.github.com>
2020-10-29 13:45:46 +0100

    Remove React from `test-compile` tests
    React has constraints that produce invalid F#:
    (See changes `test\fragments\custom\generic-type-constraints\invalid_fsharp.d.ts` for simplified examples. Examples here use React types.)
    
    ## Make generic constraint more specific
    -> `React.ReactEventHandler`
    ```typescript
    type EventHandler<E extends SyntheticEvent<any>> = (event: E) => void;
    
    type ReactEventHandler<T> = EventHandler<SyntheticEvent<T>>;
    ```
    ```fsharp
    type [<AllowNullLiteral>] EventHandler<'E when 'E :> SyntheticEvent<obj option>> =
        [<Emit "$0($1...)">] abstract Invoke: ``event``: 'E -> unit
    
    type ReactEventHandler<'T> =
        EventHandler<SyntheticEvent<'T>>
    //  ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
    //  The type 'obj option' does not match the type ''T'
    ```
    
    `EventHandler` constraints `E` to `SyntheticEvent<any>`, which is `SyntheticEvent<obj any>` in F#.
    But `ReactEventHandler` limits `any` down to `T`. That's not possible for F# `obj any` and results in an error.
    
    ## Different types for same type parameter
    -> `React.ReactHTML.head`
    ```typescript
    interface DetailedHTMLFactory<P extends HTMLAttributes<T>, T extends HTMLElement> /*...*/ {
        /*...*/
    }
    interface HTMLHeadElement extends HTMLElement { }
    
    interface ReactHTML {
        head: DetailedHTMLFactory<HTMLAttributes<HTMLElement>, HTMLHeadElement>;
    }
    ```
    ```fsharp
    type [<AllowNullLiteral>] DetailedHTMLFactory<'P, 'T when 'P :> HTMLAttributes<'T> and 'T :> HTMLElement> =
        interface end
    type [<AllowNullLiteral>] HTMLHeadElement =
        inherit HTMLElement
    
    type [<AllowNullLiteral>] ReactHTML =
        abstract head: DetailedHTMLFactory<HTMLAttributes<HTMLElement>, HTMLHeadElement> with get, set
    //                 ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
    //                 This expression was expected to have type
    //                    'HTMLElement'
    //                 but here has type
    //                    'HTMLHeadElement'
    ```
    
    ## Structural Subtyping with optional fields
    -> `React.createElement`
    ```typescript
    interface HTMLAttributes<T> {
        defaultChecked?: boolean;
        /*...*/
    }
    interface DetailedReactHTMLElement<P extends HTMLAttributes<T>, T extends HTMLElement> /*...*/ { /*...*/ }
    
    function createElement(/*...*/): DetailedReactHTMLElement<{}, HTMLInputElement>
    ```
    ```fsharp
    type [<AllowNullLiteral>] HTMLAttributes<'T> =
        abstract defaultChecked: bool option with get, set
    type [<AllowNullLiteral>] DetailedReactHTMLElement<'P, 'T when 'P :> HTMLAttributes<'T> and 'T :> HTMLElement> =
        interface end
    type [<AllowNullLiteral>] IExportsCreateElementDetailedReactHTMLElement =
        interface end
    
    type [<AllowNullLiteral>] IExports =
        abstract createElement: unit -> DetailedReactHTMLElement<IExportsCreateElementDetailedReactHTMLElement, HTMLInputElement>
    //                                  ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
    //                                  The type 'IExportsCreateElementDetailedReactHTMLElement' is not compatible with the type 'HTMLAttributes<'a>'
    ```
    Interfaces with same shape are equal in Typescript, but not in F#. Here it's a special case: `HTMLAttributes` isn't empty, but contains only optional fields. As such it can be equal `{}`. If it had just one required field it would fail in TypeScript too.

--- test/fragments/custom/generic-type-constraints/invalid_fsharp.d.ts
+++ test/fragments/custom/generic-type-constraints/invalid_fsharp.d.ts
@@ -26,11 +26,82 @@ export module ExtendOtherTypeParameter {
 export module StructuralSubtyping {
   // `A` and `B` have same shape -> are equal
   // But are different in F#
+
   interface A {}
   interface B {}
 
   interface T1<T extends A> {}
   interface T2<T extends T1<B>> {}
-  //                     ^^^^^^
+  //                     ^^^^^
   //                     The type 'B' is not compatible with the type 'A'
+}
+
+export module StructuralSubtypingWithOptionalField {
+  // Special case of the example above:
+  // `A` and `B` are different, but can have the same shape:
+  // All fields in `A` are optional -> no fields specified -> `A` is `{}`
+
+  interface A {
+    value?: number
+  }
+  interface B {}
+
+  interface T1<T extends A> {}
+  interface T2<T extends T1<B>> {}
+  //                     ^^^^^
+  //                     The type 'B' is not compatible with the type 'A'
+}
+
+export module MakeGenericMoreSpecific {
+  interface A { alpha: number }
+  interface B { beta: string }
+  interface C<T> { value: T }
+
+  interface T1<T extends C<any>> {}
+  interface S1<T extends T1<C<T>>> {}
+  //                     ^^^^^^^^
+  //                     The type 'obj option' does not match the type ''T'
+
+  interface T2<T extends C<A | B>> {}
+  interface S2<T extends T2<C<A>>> {}
+  //                     ^^^^^^^^
+  //                     The type 'U2<A,B>' does not match the type 'A'
+}
+
+export module ReduceToInvalidGenericItem {
+  // Type might be valid inside a generic, but invalid when extracted
+  // like sealed types (`Function`->`Action`) or Tuples (`A | B`):
+  // both are ok inside a generic:
+  //   * `T extends C<Function>` -> `'T when 'T :> C<Function>`
+  //   * `T extends C<A|B>` -> `T when 'T :> C<U2<A,B>>`
+  // but are removed as direct constraints: 
+  //   * `T extends Function` -> `'T`
+  //   * `T extends A | B` -> `'T`
+
+  interface A { alpha: number }
+  interface B { beta: string }
+  interface C<T> { value: T }
+
+  interface T1<T extends C<Function>> {}
+  type S1<T extends Function> = T1<C<T>>
+  //                            ^^^^^^^^
+  //                            The type 'Function' does not match the type ''T'
+
+  interface T2<T extends C<A | B>> {}
+  type S2<T extends A | B> = T2<C<T>>
+  //                         ^^^^^^^^
+  //                         The type 'U2<A,B>' does not match the type ''T'
+}
+
+export module TypeAndExtendedTypeForSameParameter {
+  interface A { alpha: number }
+  interface Aext extends A {}
+  interface C<T> { value: T }
+  
+  interface T1<T extends A, K extends C<T>> {}
+  interface S1 {
+    v: T1<Aext, C<A>>
+    // ^^^^^^^^^^^^^^
+    // The type 'Aext' does not match the type 'A'
+  }
 }
