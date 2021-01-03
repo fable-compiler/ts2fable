6f9164b5bb8ae8cd54d3c8fe2c52d962da4d65f0
BooksBaum <15612932+Booksbaum@users.noreply.github.com>
2021-01-03 21:11:49 +0100

    Merge summaries
    In jsdoc muliple tags can be used to specify a description.
    
    Add tests for summaries

--- src/transformComments.fs
+++ src/transformComments.fs
@@ -1,11 +1,54 @@
 module rec ts2fable.TransformComments
 open ts2fable.Transform
 
+let private mergeSummaries (comments: FsComment list) =
+    // there are multiple ways to specify summaries in jsdoc:
+    // * normal, top level comment: `/** my summary */`
+    //      * same as `@description`
+    // * `@description`, `@desc` 
+    //      * https://jsdoc.app/tags-description.html
+    //      * long version of `@summary`
+    // * `@summary`
+    //      * https://jsdoc.app/tags-summary.html
+    //      * short version of `@description`
+    // in c# xml comments, these are all represented as summary
+    comments
+    |> List.choose (FsComment.asSummary)
+    |> function
+       | [] -> comments
+       | [s] -> 
+            // ensure summary is at front
+            FsComment.Summary s
+            ::
+            (comments |> List.filter (not << FsComment.isSummary))
+       | ss ->
+            //todo: enhancement: `@summary` before `@description` (summary is short version of description)
+            (
+                ss
+                                               // add empty line between two summaries
+                                               // todo: enhancement: use paragraphs (`<para>...</para>`)
+                |> List.reduce (fun a b -> a @ "" :: b)
+                |> FsComment.Summary
+            )
+            ::
+            (comments |> List.filter (not << FsComment.isSummary))
+
+
+
 let transform (f: FsFile): FsFile =
 
-    //todo: implement
+    let transformComments (comments: FsComment list) =
+        comments
+        |> mergeSummaries
+
+    //todo: add other types
+    //todo: convert markdown & jsdoc link into xml
+    //todo: introduce attributes for deprecated/obsolete
     let fix ns =
         function
+        | FsType.Interface i ->
+            { i with Comments = transformComments i.Comments }
+            |> FsType.Interface
         | t -> t
 
     f
--- test/fsFileTests.fs
+++ test/fsFileTests.fs
@@ -415,6 +415,12 @@ describe "transform tests" <| fun _ ->
         let expected = "test/fragments/custom/keyof.expected.fs"
         convertAndCompareAgainstExpected tsPaths fsPath expected
 
+    it "comments/summary" <| fun _ ->
+        let tsPaths = ["test/fragments/custom/comments/summary.d.ts"]
+        let fsPath = "test/fragments/custom/comments/summary.fs"
+        let expected = "test/fragments/custom/comments/summary.expected.fs"
+        convertAndCompareAgainstExpected tsPaths fsPath expected
+
     // https://github.com/fable-compiler/ts2fable/pull/275
     it "regression #275 remove private members" <| fun _ ->
         runRegressionTest "#275-private-members"
