4ff28ec75681f4aedb792c316d33418bd620e086
BooksBaum <15612932+Booksbaum@users.noreply.github.com>
2021-03-06 18:52:54 +0100

    Fix: Source Maps (in Web App) don't work
    SourceMaps need `source-map-loader` to work with webpack.
    See: https://github.com/MangelMaxime/fulma-demo/pull/43#issuecomment-756556346

--- web-app/webpack.config.js
+++ web-app/webpack.config.js
@@ -105,6 +105,11 @@ module.exports = (env, argv) => {
             rules: [
                 {
                     test: /\.js$/,
+                    enforce: "pre",
+                    use: ["source-map-loader"],
+                },
+                {
+                    test: /\.js$/,
                     exclude: /node_modules/,
                     use: {
                         loader: 'babel-loader',
