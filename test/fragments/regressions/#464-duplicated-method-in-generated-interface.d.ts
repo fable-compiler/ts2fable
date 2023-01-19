9cdbacd5e3d6b6be6d1037abba00115d54ceb548
BooksBaum <15612932+Booksbaum@users.noreply.github.com>
2023-01-19 10:38:54 +0100

    Add example from `typescript.d.ts`

--- test/fragments/regressions/#464-duplicated-method-in-generated-interface.d.ts
+++ test/fragments/regressions/#464-duplicated-method-in-generated-interface.d.ts
@@ -3,4 +3,13 @@ export declare namespace ns {
     (arg: string): boolean
     (arg: string): boolean
   }
+
+  /**
+   * Source: https://github.com/microsoft/TypeScript/blob/e2868216f637e875a74c675845625eb15dcfe9a2/lib/typescript.d.ts#L7232-L7236
+   */
+  const createImportTypeNode: {
+      (argument: TypeNode, assertions?: ImportTypeAssertionContainer | undefined, qualifier?: EntityName | undefined, typeArguments?: readonly TypeNode[] | undefined, isTypeOf?: boolean | undefined): ImportTypeNode;
+      (argument: TypeNode, assertions?: ImportTypeAssertionContainer | undefined, qualifier?: EntityName | undefined, typeArguments?: readonly TypeNode[] | undefined, isTypeOf?: boolean | undefined): ImportTypeNode;
+      (argument: TypeNode, qualifier?: EntityName | undefined, typeArguments?: readonly TypeNode[] | undefined, isTypeOf?: boolean | undefined): ImportTypeNode;
+  };
 }
--- test/fragments/regressions/#464-duplicated-method-in-generated-interface.expected.fs
+++ test/fragments/regressions/#464-duplicated-method-in-generated-interface.expected.fs
@@ -1,5 +1,8 @@
 // ts2fable 0.0.0
 module rec ``#464-duplicated-method-in-generated-interface``
+
+#nowarn "3390" // disable warnings for invalid XML comments
+
 open System
 open Fable.Core
 open Fable.Core.JS
@@ -9,6 +12,12 @@ module Ns =
 
     type [<AllowNullLiteral>] IExports =
         abstract node: IExportsNode
+        /// <summary>Source: <see href="https://github.com/microsoft/TypeScript/blob/e2868216f637e875a74c675845625eb15dcfe9a2/lib/typescript.d.ts#L7232-L7236" /></summary>
+        abstract createImportTypeNode: IExportsCreateImportTypeNode
 
     type [<AllowNullLiteral>] IExportsNode =
         [<Emit("$0($1...)")>] abstract Invoke: arg: string -> bool
+
+    type [<AllowNullLiteral>] IExportsCreateImportTypeNode =
+        [<Emit("$0($1...)")>] abstract Invoke: argument: TypeNode * ?assertions: ImportTypeAssertionContainer * ?qualifier: EntityName * ?typeArguments: ResizeArray<TypeNode> * ?isTypeOf: bool -> ImportTypeNode
+        [<Emit("$0($1...)")>] abstract Invoke: argument: TypeNode * ?qualifier: EntityName * ?typeArguments: ResizeArray<TypeNode> * ?isTypeOf: bool -> ImportTypeNode
